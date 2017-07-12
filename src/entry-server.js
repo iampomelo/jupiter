42834366acc5f7b12c0df0e0417a88dfbab0ee16
pomelo <pomelo@meishakeji.com>
2017-07-12 20:26:19 +0800

    [Save]: third commit.

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,3 +1,17 @@
-/**
- * Created by pomelo on 2017/7/11.
- */
+import { createApp } from './app';
+
+export default context => {
+  return new Promise((resolve, reject) => {
+    const { app, router } = createApp();
+    router.push(context.url);
+    router.onReady(() => {
+      const matchedComponents = router.getMatchedComponents();
+      if (!matchedComponents.length) {
+        return reject({
+          code: 404
+        });
+      }
+      resolve(app);
+    }, reject);
+  });
+}
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+export function createStore() {
+  return new Vuex.Store({
+    state: {
+      id: ''
+    },
+    actions: {
+      setId({commit}, id) {
+        commit('SETID', {id});
+      }
+    },
+    mutations: {
+      SETID(state, {id}) {
+        state = id;
+      }
+    }
+  });
+}
